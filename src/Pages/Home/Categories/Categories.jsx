a1d81243b5d3738e51c90ae2197e29f88559f1dc
tirahman105 <tirahman105@gmail.com>
2023-08-09 23:10:35 +0600

    component added in home, Layout added, routes added

---
+++ src/Pages/Home/Categories/Categories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Categories = () => {
+    return (
+        <div>
+            this is categories section Shaon vai
+        </div>
+    );
+};
+
+export default Categories;
