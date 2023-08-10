a7fa6b83b028b1720f05921b573f2ae59392b545
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-08-10 20:18:48 +0600

    trying to make banner mobile responsive

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import logo from '../../../../public/cm-logo-png.ico';
 
 const Navbar = () => {
     return (
@@ -12,7 +12,7 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            
+
                             <li> <Link>Home</Link></li>
                             <li className="bg-white" tabIndex={0}>
                                 <details>
@@ -27,7 +27,8 @@ const Navbar = () => {
                             <li>Become Instructor</li>
                         </ul>
                     </div>
-                    <Link to="/" className="font-extrabold text-3xl">
+                    <Link to="/" className="font-extrabold text-3xl flex items-center align-middle">
+                        <img src={logo} alt="" className="w-8 h-8" />
                         CM Academy
                     </Link>
                 </div>
