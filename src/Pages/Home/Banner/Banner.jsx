15294ff55f049ba47436d729341cfa37b0833b22
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-08-10 07:42:47 +0600

    Navbar added by Raqib

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,14 +7,14 @@ import { Link } from "react-router-dom";
 const Banner = () => {
     return (
         <Carousel>
-            <div className="flex hero min-h-screen  bg-[#A5CAD3]">
+            <div className="flex hero min-h-screen  bg-gradient-to-b from-[#A5CAD3] to-cyan-300 relative">
                 <div className=" ">
                     <div className=" bg-opacity-60"></div>
                     <div className="hero-content text-left text-neutral-content">
                         <div className="">
                             <p className="text-white">Welcome to CM Academy</p>
-                            <h1 className="mb-5 text-7xl font-semibold text-white">Empowering Learning Through Technology</h1>
-                            <h1 className="mb-5 text-xl  text-black">Unleash Your Potential Today - Elevate Your Skills with CM Academy&apos;s Engaging Courses! <br />
+                            <h1 className="mb-5 text-7xl font-semibold text-white">Enriching Minds, Enabling Dreams</h1>
+                            <h1 className="mb-5 text-xl  text-black"> Embrace the Path of Unlimited Growth with CM Academy&apos;s Dynamic Learning Experience!<br />
                                 Unlock Your Potential with CM Academy&apos;s Dynamic Courses! Are you ready to dive into a world of limitless possibilities? br Welcome to CM Academy, where learning knows no bounds. </h1>
                             <p className="mb-5 text-white"></p>
                             <Link to="/" className="btn btn-success text-white p-5 m-3 ml-0">
@@ -40,8 +40,8 @@ const Banner = () => {
                     <div className="hero-content text-left text-neutral-content">
                         <div className="">
                             <p className="text-white">Welcome to CM Academy</p>
-                            <h1 className="mb-5 text-7xl font-semibold text-white">Unlock Your Potential Through Education Technology</h1>
-                            <h1 className="mb-5 text-xl  text-black">Unleash Your Potential Today - Elevate Your Skills with CM Academy&apos;s Engaging Courses! <br />
+                            <h1 className="mb-5 text-7xl font-semibold text-white">Enriching Minds, Enabling Dreams</h1>
+                            <h1 className="mb-5 text-xl  text-black"> Embrace the Path of Unlimited Growth with CM Academy&apos;s Dynamic Learning Experience!<br />
                                 Unlock Your Potential with CM Academy&apos;s Dynamic Courses! Are you ready to dive into a world of limitless possibilities? br Welcome to CM Academy, where learning knows no bounds. </h1>
                             <p className="mb-5 text-white"></p>
                             <Link to="/" className="btn btn-success text-white p-5 m-3 ml-0">
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,63 @@
-import React from 'react';
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar section by Roni vai 
-        </div>
+        <>
+
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            
+                            <li> <Link>Home</Link></li>
+                            <li className="bg-white" tabIndex={0}>
+                                <details>
+                                    <summary>Course Catagories</summary>
+                                    <ul className="p-2">
+                                        <li><a>Development</a></li>
+                                        <li><a>Design</a></li>
+                                        <li><a>Office Productivity</a></li>
+                                    </ul>
+                                </details>
+                            </li>
+                            <li>Become Instructor</li>
+                        </ul>
+                    </div>
+                    <Link to="/" className="font-extrabold text-3xl">
+                        CM Academy
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 font-semibold">
+                        <li> <Link>Home</Link></li>
+                        <li tabIndex={0}>
+                            <details>
+                                <summary>Course Catagories</summary>
+                                <ul className="p-2">
+                                    <li><a>Development</a></li>
+                                    <li><a>Design</a></li>
+                                    <li><a>Office Productivity</a></li>
+
+                                </ul>
+                            </details>
+                        </li>
+                        <li><a className="ml-auto">Become an Instructor</a></li>
+
+                    </ul>
+                </div>
+                <div className="navbar-end">
+
+                    <a className="btn btn-success text-white">Login</a>
+                </div>
+            </div>
+
+        </>
     );
 };
 
 export default Navbar;
+
