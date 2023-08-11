0d64841c3e46b9d11990848b209aecf6bc28ecdd
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-08-11 15:28:56 +0600

    banner modified by Raqib

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,9 +1,7 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Link } from "react-router-dom";
-
-
-
+import "./Banner.css"
 const Banner = () => {
     return (
         <Carousel>
