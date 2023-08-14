f3b76c0652df6c0ac8af417bb3081c793add3137
Azmal <azmalgazi8@gmail.com>
2023-08-14 06:53:28 +0600

    instructor page added by Azmal

---
+++ src/Pages/Instructors/Support/Support.jsx
@@ -0,0 +1,26 @@
+
+const Support = () => {
+    return (
+    <div className="">
+    <div className="mt-20 grid grid-cols-1 lg:grid-cols-3 items-center   gap-10">
+    <div className=" mx-auto">
+    <img src="https://s.udemycdn.com/teaching/support-1-v3.jpg" alt="" />
+    </div>  
+    <div className="w-full mx-auto text-center px-20 lg:p-4">
+    <h2 className="text-4xl font-bold">You won’t have to do it alone</h2>
+    <p className="mt-4 text-xl px-10 lg:px-4">Our <span className="font-bold">Instructor Support Team</span> is here to answer your questions and review your test video, while our Teaching Center gives you plenty of resources to help you through the process. Plus, get the support of experienced instructors in our online community.
+
+    </p>
+    <p className="mt-5 trxt-center">
+    <a className="underline text-xl text-blue-500  hover:text-blue-600 font-bold" href="">Need more details before you start? Learn more.</a>
+    </p>
+    </div>  
+    <div className="hidden lg:block">
+    <img src="https://s.udemycdn.com/teaching/support-2-v3.jpg" alt="" />
+    </div>  
+  </div>
+    </div>
+    );
+};
+
+export default Support;
