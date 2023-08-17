10ad29d3d11d3723b7ee98424321af19a241abfd
Azmal <azmalgazi8@gmail.com>
2023-08-17 06:30:25 +0600

    Add course details page, Privacy Policy and Terms and Conditions

---
+++ src/Pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,148 @@
+import { PiArrowElbowUpRightFill } from "react-icons/pi";
+
+const PrivacyPolicy = () => {
+    return (
+        <div className=" px-10 md:px-48 mt-10">
+          <h2 className="text-4xl font-bold">Privacy Policy</h2> 
+          
+          <section className="mt-10">
+          <div className="flex gap-2 items-center">
+          <PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+          <p className="text-2xl font-bold">
+          General
+          </p>
+        </div>
+
+        <p className="mt-4">
+        We,CMA Limited, incorporated under the Bangladeshi Companies Act (Act XVI), of 1994 (hereinafter referred to as “Company” or “CMA”), with the incorporation number  Npne / 2023, is committed to safeguarding your privacy in relation to the protection of your personal information. The Services are governed by this Policy, and any other rules, policies or guidelines published on the Platform as applicable to you.
+
+Please read this Policy carefully prior to accessing our Platform and using the Services. By accessing and using the Platform, providing your Personal Information (defined below), or by otherwise signaling your agreement when the option is presented to you, you consent to the collection, use, disclosure, sharing and storing of information described in this Policy, Terms of Service and any other rules, policies or guidelines published on the Platform as applicable to you (collectively referred to as the “Platform Terms”), andCMA disclaims all the liabilities arising therefrom. If you have inadvertently submitted any Personal Information toCMA prior to reading this Policy statements set out herein, or you do not agree with the way your Personal Information is collected, stored, or used, then you may access, modify and/or delete your Personal Information in accordance with this Policy.
+
+If any information you have provided or uploaded on the Platform violates the Platform Terms,CMA may be required to delete such information upon informing you of the same and revoke your access to the Platform if required. If you have any questions about this Policy, please contact us support@10minuteschool.com.
+        </p>
+          </section>
+
+          <section className="mt-10"> 
+          <div className="flex gap-2 items-center">
+          <PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+          <p className="text-2xl font-bold">
+          Controllers
+          </p>
+        </div>
+        <p className="mt-5">
+       CMA is the controller of Personal Information that it collects and processes in connection with the use of the Platform and the provision of the Services on the Platform. The kind of Personal Information we collect in connection with such use is detailed below.
+        </p>
+        </section>
+
+
+        <section className="mt-10">
+        <div className="flex gap-2 items-center">
+        <PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+        <p className="text-2xl font-bold">
+        Personal Information
+        </p>
+      </div>
+      <p className="mt-5">
+      “Personal Information” shall mean the information which identifies a User i.e., first and last name, identification number, email address, age, gender, location, photograph and/or phone number provided at the time of registration or any time thereafter on the Platform.
+
+We request you to not provideCMA with any Personal Information unless specifically requested by us. In the event you share withCMA any Personal Information without us having specifically requested for the same, then we bear no liability in respect of such Personal Information provided by you.
+      </p>
+      </section>
+
+      <section className="mt-10">
+      <div className="flex gap-2 items-center">
+      <PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+      <p className="text-2xl font-bold">
+      Information We Collect
+      </p>
+    </div>
+    <p className="mt-5">
+    We only collect information about you if we have a reason to do so — for example, to provide our Services on the Platform, to communicate with you, or to make our Services better.
+    </p>
+   
+    <p className="mt-5">We collect this information from the following sources:</p>
+
+    <p className="mt-5">Information we collect from You:</p>
+
+    <ol className="list-decimal ml-24 mt-5">
+    <li className="mt-5">Basic account information: In order to access certain features of the Platform, you will need to create an account and register with us. We ask for basic information which may include your name, an email address, state of residence, and password, along with a username and phone/mobile number.</li>
+    <li className="mt-5">Information you choose to provide: When you complete your profile within the Platform, you may choose to provide your profile photo, name of your current educational institution and current grade. We explicitly ask for your device camera permission if and when you want to update your profile picture. This is not mandatory. This information is not visible to anyone on or off the Platform.</li>
+    <li className="mt-5">Information when you communicate with us: When you write to us with a question or to ask for help, we will keep that correspondence, and the email address, for future reference; this may also include any phone/ mobile numbers if you have provided us the same as part of your communication either in writing (emails included), over a phone call or otherwise. When you browse pages on our Platform, we will track that for statistical purposes which may be to improve the Platform and the Services. You may also provide us your Personal Information or other information when you respond to surveys, enter any form of contests, tests, events, competitions, webinars, etc., hosted byCMA, either on the Platform or otherwise, or when you otherwise communicate with us via form, e-form, email, phone, or otherwise, we store a copy of our communications (including any call recordings or emails, if any, as permitted by applicable law). When you use our paid doubt solving feature, we explicitly ask for your device camera permission so that you can share any associated image with us.</li>
+    <li className="mt-5">Information related to location: You may also choose to provide location related information, including but not limited to access to GPS, which will enable us, with your consent, to offer customized offerings for specific services where location data is relevant and/or applicable such as informing you whether services on the Platform that you have expressed interest in may be availed of at or near your location.</li>
+
+    </ol>
+    <p className="mt-5">IInformation we collect automatically:
+    </p>
+    <ol className="list-decimal ml-24 mt-5">
+    <li className="mt-5">Usage information: We collect information about your usage of our Platform. We also collect information about what happens when you use our Platform (e.g., page views, support document searches, features enabled for your account, interactions with other parts of our Services) along with information about your Supported Device (e.g., screen size, name of cellular network, and mobile device manufacturer). We use this information to provide our Platform to you, get insights on how people use our Platform so that we can make our Platform better, and understand and make predictions about User retention.
+
+    </li>
+    <li className="mt-5">Location information: We may determine the approximate location of your Supported Device from your Internet Protocol (IP) address. We may collect and use this information to calculate how many people visit from certain geographic regions or to improve our Platform Services.</li>
+    <li className="mt-5">Information from cookies & other technologies: We may collect information about you through the use of cookies and other similar technologies. A cookie is a string of information that a website stores on a visitor’s computer, and that the visitor’s browser provides to the website each time the visitor returns. </li>
+    <li className="mt-5">Information related to location: You may also choose to provide location related information, including but not limited to access to GPS, which will enable us, with your consent, to offer customized offerings for specific services where location data is relevant and/or applicable such as informing you whether services on the Platform that you have expressed interest in may be availed of at or near your location.</li>
+
+    </ol>
+    </section>
+
+    <section className="mt-10">
+    <div className="flex gap-2 items-center">
+    <PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+    <p className="text-2xl font-bold">
+    Information Security
+    </p>
+  </div>
+  <p className="mt-5">
+  We work to protect the security of your information during transmission by using Transport Layer Security (TLS) or Secure Sockets Layer (SSL) software (depending on your browser/Supported Device), which encrypts information you input in addition to maintaining security of your information as required under applicable laws.
+  </p>
+  <p className="mt-5">We maintain electronic, and procedural safeguards in connection with the collection, storage, and disclosure of Personal Information (including Sensitive Personal Information). Our security procedures mean that we may occasionally request proof of identity before we disclose Personal Information to you that belongs to you.</p>
+  <p className="mt-5">However, no form or method of data storage or transmission system is fully secure, and we cannot guarantee that security provided by such system(s) is absolute and that your information will not be accessed, disclosed, or destroyed in the event of a breach of any of our security measures.</p>
+  <p className="mt-5">It is important for you to protect your account against unauthorized access to or use of your password and to your computer and if you have any reason to believe that your password has become known to anyone else, or if the password is being, or is likely to be, used in an unauthorized manner, you must immediately change your password or inform us, so that we are able to help you stop or prevent such unauthorized access. Be sure to sign off when you finish using a shared computer.</p>
+   
+  </section>
+
+  <section className="mt-10">
+  <div className="flex gap-2 items-center">
+  <PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+  <p className="text-2xl font-bold">
+  Promotional Communications
+  </p>
+</div>
+<p className="mt-5">You will always have the option to opt out of receiving some or all of our promotional communications by using the unsubscribe link in any email communications sent to you or by emailing support@cmacademy.com.</p>
+<p className="mt-5">If you opt out of promotional communications, we may still send you transactional communications, such as service announcements, administrative and legal notices, and information about your account, without offering you the opportunity to opt out of these communications. If you no longer wish to use our Platform or receive any communications from us (except for those that are legally required), then you may delete your account by using the option enabled within the Platform or by writing to us at support@cmacademy.com.</p>
+<p className="mt-5">Advertisers or ad companies working on their behalf sometimes use technology to serve the ads that appear on our sites directly to Your browser. They automatically receive Your IP address when this happens. They may also use cookies to measure the effectiveness of their ads and to personalize ad content. We do not have access to or control over cookies or other features that advertisers and third-party sites may use, and the information practices of these advertisers and third-party websites are not covered by our Policy. Please contact them directly for more information about their privacy practices.</p>
+<p className="mt-5">Please note that opting out of promotional email communications only affects future communications from us. If we have already provided your information to a third party (as stated in this Policy) before you changed your preferences or updated your information, you may have to change your preferences directly with that third party.</p>
+<p className="mt-5"> We do not sell your Personal Information to third parties, and we do not use your name or name of your company in marketing statements without your consent.</p>
+</section>
+
+
+<section className="mt-10">
+<div className="flex gap-2 items-center">
+<PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+<p className="text-2xl font-bold">
+Modification to Privacy Policy
+</p>
+</div>
+<p className="mt-5">
+Our business changes constantly and our Policy may change from time to time. We may, at our discretion (unless required by applicable laws to mandatorily do so), email periodic reminders of our notices and conditions, unless you have instructed us not to, but we encourage you to check our Platform frequently to see the recent changes. Unless stated otherwise, our current Policy applies to all information that we have about you and your account. We stand behind the promises we make, however, and will not materially change our policies and practices making them less protective of customer information collected in the past without your consent.
+</p>
+</section>
+
+<section className="mt-10">
+<div className="flex gap-2 items-center">
+<PiArrowElbowUpRightFill className="text-3xl text-blue-500 mt-3"></PiArrowElbowUpRightFill>
+<p className="text-2xl font-bold">
+Privacy Grievances
+</p>
+</div>
+<p className="mt-5">
+If you have any questions about this Policy, wish to exercise your rights, have concerns about privacy of your data or any privacy related grievances in respect of the Platform, then please register your complaint with a thorough description via email to support@cmacademy.com or call our hotline at 16910.
+</p>
+</section>
+
+  
+            
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
---
+++ src/Pages/TermsAndConditions/TermsAndConditions.jsx
@@ -0,0 +1,77 @@
+
+ 
+ const TermsAndConditions = () => {
+    return (
+        <div className=" px-10 md:px-48 mt-10">
+        <h2 className="text-4xl font-bold">TERMS AND CONDITIONS</h2> 
+            <div>
+            <p className="mt-10">These Terms & Conditions (“ <b>Terms</b> ”) of (a) use of our websitewww.10minuteschool.com  (“ <b>Website</b> ”), our applications (“Application”) or any products or services in connection with the Application/, Website/products (“ <b>Services</b>”)  or (b) any modes of registrations or usage of products, including through SD cards, tablets or other storage/transmitting device  are between 10MS Limited (“ <b>Company/We/Us/Our</b>”) and its users (“ <b>User/You/Your</b>”).
+
+            Please carefully read the terms and privacy policy of the Company (“ <b>Privacy Policy</b>”) prior to registering, using the application, website, or services. Any discrepancies between the Terms and other policies on the application, website, or service are settled in favor of the Terms.
+            
+            You accept and agree to be legally bound by the Terms if you access the Application, Website, or Services through any means.
+            
+            To avoid any potential conflicts, please refrain from using the application, website, or services if you have any issues with the terms or privacy policy. Our Application along with all our services and products can only be accessed through a registration/subscription. </p>
+            
+            </div>
+
+
+            <ol className="list-disc   mt-5">
+            <li className="mt-5">The software, text, images, graphics, video, script, and audio, including any other information, content, material, trademarks, service marks, trade names, and trade secrets, all of which are contained in the Application, Website, and services and products, are the Company proprietary property (“Proprietary Information”). Permission to reproduce, reuse, modify, and distribute Proprietary Information may not be granted without first obtaining permission from the Company. Moreover, no User of this Application, Website, or Services may claim any ownership in or to the intellectual property rights of the Company. You can keep control of the contents, but the company always maintains ownership of the information, content, and all intellectual property on that medium. While using the Website, you may encounter materials that belong to other people. To ensure that the content is faithful to the original, prior consent was obtained from the subject and all rights to the content belong to the third party. Furthermore, you accept that all intellectual property, including trademarks, copyrights, logos, and service marks, remains the property of the respective third party, and you are not entitled to use it without the third partys permission.</li>
+            <li className="mt-5">It is prohibited to use our products, services, website, and application for any purpose other than personal or non-commercial. It is prohibited to use the application, website, services, or products for any purpose except personal ones. The following rules apply to your use of this application, website, and our services for personal and non-commercial purposes:
+            i. You may not copy, modify, translate, display, distribute, or perform any information or software obtained from the Application and / or our Website and/or Services/Products or remove any copyright, trademark registration, or other proprietary notices from the contents of the Application and / or and / or our Website and/or Services/Products.
+            
+            ii. You are prohibited from using this application, our website, and our products and services for commercial purposes, including but not limited to selling, advertising, or promoting the application or any of our products or services, asking for contributions or donations, or using public forums for commercial purposes.
+            </li>
+            <li className="mt-5">Make available any deceptive, unlawful, hazardous, abusive, torturing, defamatory, libelous, vulgar, obscene, child pornographic, loud, lascivious, unlawful, intrusive, hateful, racial, ethnic or otherwise objectionable content;</li>
+            <li className="mt-5">Threatening, stalking, and/or harassing another person, as well as inciting another person to conduct violence;</li>
+            <li className="mt-5">Passing on information that encourages illegal acts, that results in civil liability, or that violates any applicable laws, regulations, or guidelines;</li>
+            <li className="mt-5">Doing anything that interferes with another persons usage or enjoyment of the Application/Website/Services;</li>
+            <li className="mt-5">To engage in making, copying, transmitting, storing, or otherwise reproducing any intellectual property protected under copyright law, doing anything that amounts to an infringement of intellectual property, or providing others with access to anything that infringes on any intellectual property or other proprietary rights of any party;</li>
+            <li className="mt-5">Falsely impersonate any person or entity, or misrepresent your relationship with any person or entity;</li>
+            <li className="mt-5">The legal matters of this contract shall be determined in accordance with the laws of Bangladesh, regardless of conflicts of law. In addition, these terms will be resolved in the jurisdiction of the relevant courts in Dhaka, and you agree to accept their authority.</li>
+            <li className="mt-5">The firm is able to make any adjustments, cease, or eliminate any part of the application or service whenever it sees fit without prior warning. In addition, the Company is entitled to alter these Terms from time to time and without notice. There is no promise from the business that they will continue to run or keep any feature of the Application. The Company does not owe you or any other third party compensation for modifications, interruptions, or terminations of the Application/Services. Prices may change at any time.</li>
+            <li className="mt-5">DISCLAIMER: THIS WEBSITE, THE APPLICATION AND THE SERVICES ARE PROVIDED ON AN "AS IS" BASIS WITH ALL FAULTS AND WITHOUT ANY WARRANTY OF ANY KIND. THE COMPANY HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS WITH REGARD TO THE WEBSITE, APPLICATION/PRODUCTS AND THE SERVICES, INCLUDING WITHOUT LIMITATION, ALL IMPLIED WARRANTIES AND CONDITIONS OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE, ACCURACY, TIMELINESS. PERFORMANCE, COMPLETENESS, SUITABILITY AND NON-INFRINGEMENT. ADDITIONALLY, THE COMPANY SHALL NOT BE LIABLE FOR ANY DAMAGES ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SITE, OR THE APPLICATION OR THE SERVICES. YOUR USE OF ANY INFORMATION OR MATERIALS ON THIS WEBSITE/APPLICATION/SERVICES/PRODUCTS IS ENTIRELY AT YOUR OWN RISK, FOR WHICH WE SHALL NOT BE LIABLE. IT SHALL BE YOUR OWN RESPONSIBILITY TO ENSURE THAT SERVICES PROVIDED BY US MEET YOUR SPECIFIC REQUIREMENTS.</li>
+
+            <li className='mt-5'>
+            General Provisions:
+
+           <div className='ml-3 mt-3'>
+ i. To ensure that any legal notifications are properly sent, please note that the Company will use your email address and/or the application to notify you. The Company should be notified at support@cmacademy.com. 
+ ii. the entire contract The Terms, the Privacy Policy, and any other rules deemed relevant to the Application apply to you and your use of the Application, Website, and Services.
+
+iii. The Terms cannot be transferred or assigned to any other party. The Company may freely transfer its rights under the Terms to any other parties without needing your permission.
+
+iv. If any provision of the Terms is deemed unenforceable, the remaining terms shall continue in full force and effect.
+
+v. The Company will not waive any of its rights under the Terms by failing to enforce or exercise them.
+
+vi. You agree that by being a part of the service, you are not a company employee, agency, partnership, or franchise.
+
+vii. To make sure you are aware of the terms that apply to your use of the website/application and services, the Company provides these terms. 
+
+viii. You have agreed to these terms after the company provided you with a sufficient amount of time to evaluate them.
+           </div>
+            
+            </li>
+        
+            <li className="mt-5">
+            Feedback
+            <p>Your information, provided about the Application, will be considered non-confidential. Applications can use the information whatever they choose. In addition, you represent and warrant by sending feedback that (i) your feedback does not contain confidential or patent information from you or third parties; (ii) the Company is not obligated or implied to maintain confidentiality regarding feedback; (iii) the Application could be similar to the feedback already being considered or under development;
+
+            The Company is not liable for anything published by other users, including inappropriate or harmful content. It is important to be careful when visiting the Application.</p>
+            </li>
+
+            <li className="mt-5">Customer service
+            <p>Well do all in our power to make your experience enjoyable. If you run into any problems, please get in touch with us at support@10minuteschool.com.</p>
+            </li>
+            </ol>
+
+            
+
+        
+          </div>
+    );
+ };
+ 
+ export default TermsAndConditions;
