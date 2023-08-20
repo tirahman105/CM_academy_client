5591c6c0bd384037a2524c56a0831f49bbbd5966
samsulalamasif <samsulalamasif@gmail.com>
2023-08-20 06:45:55 +0600

    added loader, banner image resize by asif

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {
-  
+
   RouterProvider,
 } from "react-router-dom";
 import router from './Routes/Routes';
@@ -13,6 +13,6 @@ import AuthProvider from './providers/AuthProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <AuthProvider><RouterProvider router={router} /></AuthProvider>
+    <AuthProvider><RouterProvider router={router} /></AuthProvider>
   </React.StrictMode>,
 )
