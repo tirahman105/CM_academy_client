338ca2f4a2d47fb48d1bf782a66d24db813505d9
Azmal <azmalgazi8@gmail.com>
2023-08-22 22:38:01 +0600

    Update Course Detailspage, Course page, Instructor page

--- src/Pages/Instructors/Banner/Banner.jsx
+++ src/Pages/Instructors/Banner/Banner.jsx
@@ -1,21 +1,17 @@
-import { Link } from "react-router-dom";
-
  
-// import tarekVi from '../../../assets/tarek_vi.png' 
 const Banner = () => {
     return (
-        <div className="bg-[#A5CAD3] flex md:flex-row flex-col-reverse    gap-10 justify-around items-center p-10">
-
-        <div>
-         <h2 className="text-5xl font-bold text-white ">Come teach <br /> with us</h2>
-         <p className="text-2xl mt-2">Become an instructor and change <br /> lives — including your own</p>
-         <Link to="/instructorSignUp"><button className=" mt-4 bg-black w-80 p-4 text-xl text-white font-bold">Get Started</button></Link>
+        <div className="bg-[#A5CAD3] flex flex-col-reverse md:flex-row gap-10 justify-around items-center p-10">
+        <div className="md:w-1/2">
+            <h2 className="text-4xl md:text-5xl font-bold text-white mb-4 md:mb-0">Come teach<br /> with us</h2>
+            <p className="text-lg md:text-2xl mt-2">Become an instructor and change<br /> lives — including your own</p>
+            <button className="mt-4 md:mt-6 bg-black w-full md:w-80 p-3 md:p-4 text-lg md:text-xl text-white font-bold">Get Started</button>
         </div>
-        <div>
-         <img className="w-full" src="https://i.ibb.co/BKqkFv0/Teach-with-us2.png" alt="" />
+        <div className="md:w-1/2">
+            <img className="w-full" src="https://i.ibb.co/BKqkFv0/Teach-with-us2.png" alt="" />
         </div>
-            
-        </div>
+    </div>
+    
     );
 };
 
--- src/Pages/Instructors/Review/Review.jsx
+++ src/Pages/Instructors/Review/Review.jsx
@@ -47,11 +47,10 @@ const Review = () => {
   ];
 
   return (
-    <div className="container mx-auto py-8">
-      <h2 className="text-3xl md:text-5xl text-center font-bold mb-20"> Instructors Review</h2>
-      <Slider items={items} />
-    </div>
+    <div className="container mx-auto px-4 md:px-6 lg:px-8 py-8">
+    <Slider items={items} />
+</div>
   );
 };
 
-export default Review;
+export default Review; 
