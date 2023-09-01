dd038333a95ce1fe23977cbe8543c79936007775
DShaon <toxicshaon@gmail.com>
2023-09-01 16:30:59 +0600

    add dynamic data to mycourse update page

--- src/Pages/CoursePage/CoursePageUpdate.jsx
+++ src/Pages/CoursePage/CoursePageUpdate.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const CoursePageUpdate = () => {
-    return (
-        <div>
-            This is course page after enrollment. 
-        </div>
-    );
+  const location = useLocation();
+  const { course } = location.state;
+
+  console.log(course);
+  return (
+    <div className="pt-24">
+      This is course page after enrollment.
+      <div>
+        <h1 className="text-6xl">{course.title}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default CoursePageUpdate;
