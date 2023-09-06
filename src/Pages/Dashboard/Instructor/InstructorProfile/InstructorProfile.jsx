44f346450f3a9a14314464d3a7504253f3e3102f
tirahman105 <tirahman105@gmail.com>
2023-09-06 20:17:18 +0600

    instructor profile added and home why us update

---
+++ src/Pages/Dashboard/Instructor/InstructorProfile/InstructorProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InstructorProfile = () => {
+    return (
+        <div>
+            <h1>Instructor profile </h1>
+        </div>
+    );
+};
+
+export default InstructorProfile;
