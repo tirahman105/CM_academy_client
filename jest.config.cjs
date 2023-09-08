a558fbbba7ea065471c32a84379b3e0348d9d78f
DShaon <toxicshaon@gmail.com>
2023-09-08 14:59:28 +0600

    changes in authprovider

---
+++ jest.config.cjs
@@ -0,0 +1,40 @@
+// jest.config.cjs
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/*.test.(ts|tsx|js|jsx)'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*.stories.tsx',
+    '!src/**/index.ts',
+    '!src/**/*.test.{ts,tsx,js,jsx}',
+  ],
+
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'], 
+};
+
+
+// module.exports = {
+//   testEnvironment: "jsdom",
+//   transform: {
+//     "^.+\\.jsx?$": "babel-jest"
+//   },
+//   testMatch: [
+//     "**/__tests__/**/*.jsx?(x)",
+//     "**/?(*.)+(spec|test).jsx?(x)"
+//   ],
+//   transformIgnorePatterns: [
+//     "/node_modules/(?!(module-to-ignore)/)"
+//   ],
+//   moduleNameMapper: {
+//     "\\.(css|less)$": "identity-obj-proxy"
+//   }
+// };
