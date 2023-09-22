62a7396336a85e39aed3cb84e050ccfd392c318c
toushik018 <tayebhossain018@gmail.com>
2023-09-22 14:00:19 +0600

    make responsive the blog and blog details page

--- src/Pages/Blogs/BlogDetails.jsx
+++ src/Pages/Blogs/BlogDetails.jsx
@@ -39,7 +39,7 @@ const BlogDetails = () => {
 
   return (
     <div className="bg-gray-100">
-      <div className="container mx-auto p-6">
+      <div className="mx-auto p-4">
         <div className="py-4 mt-16">
           <p className="text-gray-500">
             <Link to="/allBlog" className="text-blue-500 cursor-pointer hover:underline">
